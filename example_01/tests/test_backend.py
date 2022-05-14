7b074a02e573fd17b5da34626a7e3f050bc975da
Julio_Paiva <julio.paiva@luizalabs.com>
2022-05-14 17:51:06 -0300

    refact conditional play type for a new function

--- example_01/tests/test_backend.py
+++ example_01/tests/test_backend.py
@@ -1,5 +1,5 @@
 import pytest
-from example_01.backend import statement
+from example_01.backend import statement, amount_for
 
 
 def test_statement_should_return_success(invoice, plays):
@@ -24,3 +24,32 @@ def test_statement_should_return_error_with_type_invalid(invoice):
 
     with pytest.raises(Exception):
         statement(invoice, plays)
+
+
+@pytest.mark.parametrize('a_perm, play, expected', [
+    (-5, 'tragedy', 40000),
+    (-5, 'comedy', 28500),
+    (0, 'tragedy', 40000),
+    (0, 'comedy', 30000),
+    (10, 'tragedy', 40000),
+    (10, 'comedy', 33000),
+    (50, 'tragedy', 60000),
+    (50, 'comedy', 61000),
+])
+def test_amount_for_should_return_success(a_perm, play, expected):
+    _play = {'type': play}
+    result = amount_for(a_perm, _play)
+
+    assert result == expected
+
+
+def test_amount_for_should_return_error():
+    play = {
+        "hairspray": {
+            "name": "Wheee",
+            "type": "blablabla"
+        }
+    }
+
+    with pytest.raises(Exception):
+        amount_for(50, play)
