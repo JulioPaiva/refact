c51f360a77fd4db6e4f0a1fb4084ec3887e8bde6
Julio_Paiva <julio.paiva@luizalabs.com>
2022-05-15 15:03:19 -0300

    adding format_real function

--- example_01/tests/test_backend.py
+++ example_01/tests/test_backend.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest import mock
-from example_01.backend import statement, amount_for, play_for
+from example_01.backend import statement, amount_for, play_for, format_real
 
 
 @mock.patch('example_01.backend.play_for')
@@ -12,8 +12,8 @@ def test_statement_should_return_success(mock, invoice, plays):
 
     assert result == (
         'Statement for LittleCo\n'
-        'Hairspray: 570.0 (45 seats)\n'
-        'Amount owed is 570.0\n'
+        'Hairspray: R$ 570.00 (45 seats)\n'
+        'Amount owed is R$ 570.00\n'
         'You earned 24 credits\n'
     )
 
@@ -47,3 +47,13 @@ def test_play_for_should_return_success():
 
     assert result['name']
     assert result['type']
+
+
+@pytest.mark.parametrize('number, expected', [
+    (-100, 'R$ -1.00'), (0, 'R$ 0.00'), (200, 'R$ 2.00'), (200.500, 'R$ 2.00'),
+    (1000, 'R$ 10.00'), (1000.1, 'R$ 10.00'), (1010.123456789, 'R$ 10.10')]
+)
+def test_format_real_should_return_success(number, expected):
+    result = format_real(number)
+
+    assert result == expected
